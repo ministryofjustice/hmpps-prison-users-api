0a77336e373190d8034c18258cd64fd18b0cf630
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2026-08-26 10:58:10 +0100

    HAAR-5968: ktlint format corrections

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonusersapi/service/converters/FromPrisonUserSyncRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonusersapi/service/converters/FromPrisonUserSyncRequest.kt
@@ -6,34 +6,32 @@ import uk.gov.justice.digital.hmpps.prisonusersapi.jpa.User
 import uk.gov.justice.digital.hmpps.prisonusersapi.jpa.UserEmail
 import uk.gov.justice.digital.hmpps.prisonusersapi.service.PrimaryEmailDetector
 
-fun PrisonUserSyncRequest.toUser(legacyStaffId: Long): User {
-    return User(
-        firstName = firstName,
-        lastName = lastName,
-        status = status,
-        legacyStaffId = legacyStaffId,
-        createdTimestamp = createdTimestamp,
-        createdBy = createdBy,
-        modifiedTimestamp = modifiedTimestamp,
-        modifiedBy = modifiedBy,
-        userEmails = mutableListOf(),
-        )
-}
+fun PrisonUserSyncRequest.toUser(legacyStaffId: Long): User = User(
+  firstName = firstName,
+  lastName = lastName,
+  status = status,
+  legacyStaffId = legacyStaffId,
+  createdTimestamp = createdTimestamp,
+  createdBy = createdBy,
+  modifiedTimestamp = modifiedTimestamp,
+  modifiedBy = modifiedBy,
+  userEmails = mutableListOf(),
+)
 
 fun PrisonUserSyncRequest.addEmailsTo(user: User, primaryEmailDetector: PrimaryEmailDetector): User {
-    val emails: List<SyncPrisonUserEmail> = this.emails.sortedBy { it.email }
-    val primaryEmail: String? = primaryEmailDetector.getPrimaryEmail(emails)
-    emails.forEach {
-        user.addUserEmail(
-            UserEmail(
-                email = it.email,
-                isPrimary = it.email == primaryEmail,
-                createdBy = it.createdBy,
-                createdTimestamp = it.createdTimestamp,
-                user = user,
-                ),
-            )
-        }
+  val emails: List<SyncPrisonUserEmail> = this.emails.sortedBy { it.email }
+  val primaryEmail: String? = primaryEmailDetector.getPrimaryEmail(emails)
+  emails.forEach {
+    user.addUserEmail(
+      UserEmail(
+        email = it.email,
+        isPrimary = it.email == primaryEmail,
+        createdBy = it.createdBy,
+        createdTimestamp = it.createdTimestamp,
+        user = user,
+      ),
+    )
+  }
 
-    return user
+  return user
 }
